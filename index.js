76b5de60475453cee88beb048ab56c24d239b73d
Dardanboy <dardan.iljazi@gmail.com>
2020-02-13 12:19:16 +0100

    Finish working code

--- index.js
+++ index.js
@@ -3,14 +3,36 @@ var express = require("express");
 var app = express();
 const port = 2000;
 
-app.get("/", function(req, res) {
+// Param: Bucket et name de l'image (get)
+app.get('/api/imagerecognition', (req, res) => {
   AWS.config.loadFromPath("./config.json");
+
+  // Responses are in json format
+  res.setHeader('Content-Type', 'application/json');
+
+  var bucket = req.query.bucket;
+
+
+  if (bucket == null)
+  {
+    console.error('[WARNING]: bucket is empty');
+    res.status(400).send(JSON.stringify({"error" : 'Invalid request, bucket must be provided'}));
+  }
+
+  var filename = req.query.filename;
+
+
+  if (filename === null)
+  {
+    console.error('[WARNING]: fileName is empty');
+    res.status(400).send(JSON.stringify({"error" : 'Invalid request, fileName must be provided'}));
+  }
 
   var params = {
     Image: {
       S3Object: {
-        Bucket: "rekognition.actualit.info",
-        Name: "Emirates-A380.jpg"
+        Bucket: bucket,
+        Name:  filename
       }
     },
     MaxLabels: 1,
@@ -20,9 +42,10 @@ app.get("/", function(req, res) {
   rekognition.detectLabels(params, function(err, data) {
     if (err) console.log(err, err.stack);
     // an error occurred
-    else console.log(data);
+    else res.send(JSON.stringify(data));
   }); // successful response
-  res.send("hello world !");
+
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port http://localhost:${port}/api/imagerecognition`))
+
