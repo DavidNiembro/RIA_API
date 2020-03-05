fabdf11c04735591960f7f8a8e8ec225897dfc71
Dardanboy <dardan.iljazi@gmail.com>
2020-03-05 15:18:13 +0100

    Finish tests

--- test/test.js
+++ test/test.js
@@ -6,7 +6,6 @@ var assert = require('assert')
 var AwsLabelDetectorImpl = require("../src/models/AwsLabelDetectorImpl");
 
 
-
 describe("AWSRekognitionTests", function() {
   let awsDetector = new AwsLabelDetectorImpl();
   // BUCKET TEST
@@ -17,46 +16,15 @@ describe("AWSRekognitionTests", function() {
     });
   });
 
-  // BUCKET TEST
-  it('[BUCKET] should be equal to the rekognitionJsonResult.json file', function(done) {
-    awsDetector.MakeAnalysisRequest("aws.rekognition.actualit.info", getFileResultRaw('./test/emiratesa380.jpg'), 1, function(data) {
+  //BUCKET TEST
+  it('[LOCAL] should be equal to the rekognitionJsonResult.json file', function(done) {
+    awsDetector.MakeAnalysisRequestLocal('./test/emiratesa380.jpg', 1, function(data) {
       assert.equal(data, getFileResult('rekognitionJsonResult.json'))
       done();
     });
   });
-
-
-
-  //
-  // // LOCAL TEST
-  // it('[LOCAL] should be equal to the rekognitionJsonResult.json file', function(done) {
-  //   this.timeout(10000)
-  //   // console.log(Buffer.from(getFileResult('./test/emiratesa380.jpg')).toString('base64'))
-  //   var params = {
-  //     Image: {
-  //       "Bytes": getFileResultRaw('./test/emiratesa380.jpg'),
-  //     },
-  //     MaxLabels: 1,
-  //     MinConfidence: 90
-  //   };
-  //
-  //   rekognition.detectLabels(params, function(err, data) {
-  //     if (err)
-  //     {
-  //       console.log(err, err.stack);
-  //     }
-  //     // an error occurred
-  //     else
-  //     {
-  //       let jsonObject = parseJson(JSON.stringify(data));
-  //       let jsonStringified = stringifyJson(jsonObject);
-  //       assert.equal(jsonStringified, getFileResult('rekognitionJsonResult.json'))
-  //       done();
-  //     }
-  //   });
-  // });
-
 });
+
 
 
 function parseJson(jsonText)
