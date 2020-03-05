99b0ff2e1f4cbf4715d4c2a3d3bfcc3e94717249
Dardanboy <dardan.iljazi@gmail.com>
2020-03-05 15:01:13 +0100

    Add class model implementation

---
+++ src/models/AwsLabelDetectorImpl.js
@@ -0,0 +1,34 @@
+var AWS = require("aws-sdk");
+
+class AwsLabelDetectorImpl {
+    constructor() {
+        AWS.config.loadFromPath("./config.json");
+        this.rekognition = new AWS.Rekognition();
+    }
+
+    MakeAnalysisRequest(bucketName, dataObjectName, maxLabels, callback) {
+        this.ApiRequest(bucketName, dataObjectName, maxLabels, callback);
+    }
+
+    ApiRequest(bucketName, dataObjectName, maxLabels, callback) {
+        var params = {
+            Image: {
+                Bytes: dataObjectName,
+                S3Object: {
+                    Bucket: bucketName,
+                    Name: dataObjectName
+                }
+            },
+            MaxLabels: maxLabels
+        };
+        this.rekognition.detectLabels(params, function(err, data) {
+            if (err) console.log(err, err.stack);
+            else {
+                // console.log(data);
+                callback(JSON.stringify(data));
+            }
+        });
+    }
+}
+
+module.exports = AwsLabelDetectorImpl;
--- test/test.js
+++ test/test.js
@@ -3,70 +3,58 @@ var chai = require("chai")
 var fs = require('fs');
 var http = require('http');
 var assert = require('assert')
+var AwsLabelDetectorImpl = require("../src/models/AwsLabelDetectorImpl");
 
-
-
-AWS.config.loadFromPath("./config.json");
-var rekognition = new AWS.Rekognition();
 
 
 describe("AWSRekognitionTests", function() {
+  let awsDetector = new AwsLabelDetectorImpl();
   // BUCKET TEST
   it('[BUCKET] should be equal to the rekognitionJsonResult.json file', function(done) {
-    var params = {
-      Image: {
-        S3Object: {
-          Bucket: 'aws.rekognition.actualit.info',
-          Name: 'emiratesa380.jpg'
-        }
-      },
-      MaxLabels: 1,
-      MinConfidence: 90
-    };
-
-    rekognition.detectLabels(params, function(err, data) {
-      if (err)
-      {
-        console.log(err, err.stack);
-      }
-      // an error occurred
-      else
-      {
-        let jsonObject = parseJson(JSON.stringify(data));
-        let jsonStringified = stringifyJson(jsonObject);
-        assert.equal(jsonStringified, getFileResult('rekognitionJsonResult.json'))
-        done();
-      }
+    awsDetector.MakeAnalysisRequest("aws.rekognition.actualit.info", "emiratesa380.jpg", 1, function(data) {
+      assert.equal(data, getFileResult('rekognitionJsonResult.json'))
+      done();
     });
   });
 
-  // LOCAL TEST
-  it('[LOCAL] should be equal to the rekognitionJsonResult.json file', function(done) {
-    this.timeout(10000)
-    // console.log(Buffer.from(getFileResult('./test/emiratesa380.jpg')).toString('base64'))
-    var params = {
-      Image: {
-        "Bytes": getFileResultRaw('./test/emiratesa380.jpg'),
-      },
-      MaxLabels: 1,
-      MinConfidence: 90
-    };
-
-    rekognition.detectLabels(params, function(err, data) {
-      if (err)
-      {
-        console.log(err, err.stack);
-      }
-      // an error occurred
-      else
-      {
-        let jsonObject = parseJson(JSON.stringify(data));
-        let jsonStringified = stringifyJson(jsonObject);
-        assert.equal(jsonStringified, getFileResult('rekognitionJsonResult.json'))
-        done();
-      }
+  // BUCKET TEST
+  it('[BUCKET] should be equal to the rekognitionJsonResult.json file', function(done) {
+    awsDetector.MakeAnalysisRequest("aws.rekognition.actualit.info", getFileResultRaw('./test/emiratesa380.jpg'), 1, function(data) {
+      assert.equal(data, getFileResult('rekognitionJsonResult.json'))
+      done();
     });
   });
+
+
+
+  //
+  // // LOCAL TEST
+  // it('[LOCAL] should be equal to the rekognitionJsonResult.json file', function(done) {
+  //   this.timeout(10000)
+  //   // console.log(Buffer.from(getFileResult('./test/emiratesa380.jpg')).toString('base64'))
+  //   var params = {
+  //     Image: {
+  //       "Bytes": getFileResultRaw('./test/emiratesa380.jpg'),
+  //     },
+  //     MaxLabels: 1,
+  //     MinConfidence: 90
+  //   };
+  //
+  //   rekognition.detectLabels(params, function(err, data) {
+  //     if (err)
+  //     {
+  //       console.log(err, err.stack);
+  //     }
+  //     // an error occurred
+  //     else
+  //     {
+  //       let jsonObject = parseJson(JSON.stringify(data));
+  //       let jsonStringified = stringifyJson(jsonObject);
+  //       assert.equal(jsonStringified, getFileResult('rekognitionJsonResult.json'))
+  //       done();
+  //     }
+  //   });
+  // });
 
 });
 
