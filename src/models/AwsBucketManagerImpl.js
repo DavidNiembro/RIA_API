8402eb057ab1a5e33e1d972c370ca15fd47f8131
David Niembro Scaccianoce <david@niembro.ch>
2020-03-12 15:51:04 +0100

    fix name

--- src/models/AwsBucketManagerImpl.js
+++ src/models/AwsBucketManagerImpl.js
@@ -1,63 +1,50 @@
 var AWS = require("aws-sdk");
 var fs = require("fs");
 
+class AwsBucketManagerImpl {
+  /**
+   * Constructor that returns a new instance of BucketManagerImpl class
+   * @param bucketUrl
+   */
+  constructor(bucketUrl) {
+    // AWS.config.loadFromPath("./config.json");
+    // this.bucketUrl = bucketUrl; // "aws.rekognition.actualit.info";
+    //
+    // // Create the instance as in the doc (don't know  yet why apiVersion is set here):
+    // // https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/s3-example-creating-buckets.html
+    // this.client = new AWS.S3({apiVersion: '2006-03-01'});
+  }
 
-class BucketManagerImpl {
+  /**
+   * Method that creates the bucket
+   * @param objectUrl
+   * @param filePath
+   * @constructor
+   */
+  CreateObject(objectUrl, filePath = "") {}
 
-    /**
-     * Constructor that returns a new instance of BucketManagerImpl class
-     * @param bucketUrl
-     */
-    constructor(bucketUrl) {
-        // AWS.config.loadFromPath("./config.json");
+  /**
+   * Method that download the data object
+   * @param objectUrl
+   * @param destinationUrl
+   * @param update
+   * @constructor
+   */
+  DownloadObject(objectUrl, destinationUrl, update = false) {}
 
-        // this.bucketUrl = bucketUrl; // "aws.rekognition.actualit.info";
-        //
-        // // Create the instance as in the doc (don't know  yet why apiVersion is set here):
-        // // https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/s3-example-creating-buckets.html
-        // this.client = new AWS.S3({apiVersion: '2006-03-01'});
-    }
+  /**
+   * Method that checks if objectUrl is pointing to something valid (existing)
+   * @param objectUrl
+   * @constructor
+   */
+  IsObjectExists(objectUrl) {}
 
-    /**
-     * Method that creates the bucket
-     * @param objectUrl
-     * @param filePath
-     * @constructor
-     */
-    public CreateObject(objectUrl, filePath = "") {
-
-    }
-
-    /**
-     * Method that download the data object
-     * @param objectUrl
-     * @param destinationUrl
-     * @param update
-     * @constructor
-     */
-    public DownloadObject(objectUrl, destinationUrl, update = false) {
-
-    }
-
-    /**
-     * Method that checks if objectUrl is pointing to something valid (existing)
-     * @param objectUrl
-     * @constructor
-     */
-    public IsObjectExists(objectUrl) {
-
-    }
-
-    /**
-     * Remove the bucket or a file in the bucket
-     * @param objectUrl
-     * @constructor
-     */
-    public RemoveObject(objectUrl) {
-
-    }
-
-
+  /**
+   * Remove the bucket or a file in the bucket
+   * @param objectUrl
+   * @constructor
+   */
+  RemoveObject(objectUrl) {}
 }
 
-module.exports = AwsLabelDetectorImpl;
+module.exports = AwsBucketManagerImpl;
