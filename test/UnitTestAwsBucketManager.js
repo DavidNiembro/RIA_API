0e57258f0f624dfcceda1a6b2597d2688a8174dd
David Niembro Scaccianoce <david@niembro.ch>
2020-03-12 15:35:18 +0100

    Add all test name

--- test/UnitTestAwsBucketManager.js
+++ test/UnitTestAwsBucketManager.js
@@ -1,13 +1,53 @@
 var assert = require("assert");
 
+let bucketName = null;
+let domain = null;
+let bucketUrl = null;
+let imageName = null;
+let fullPathToImage = null;
+let prefixObjectDownloaded = null;
+let bucketManager = null;
+
 describe("UnitTestAwsBucketManager", function() {
+  beforeEach(function() {
+    bucketName = "testbucket";
+    domain = "dev.actualit.info";
+    bucketUrl = bucketName + "." + domain;
+    imageName = "emiratesa380.jpg";
+    fullPathToImage = "./test/" + imageName;
+    prefixObjectDownloaded = "downloaded";
+    //bucketManager = new AwsBucketManagerImpl(bucketUrl);
+  });
+
   // BUCKET TEST
-  it("[BUCKET] should be equal to the rekognitionJsonResult.json file", function(done) {
+
+  it("CreateObject_CreateNewBucket_Success", function(done) {
     assert.equal(true, true);
     done();
   });
-
-  afterEach(function() {
-    // runs after each test in this block
+  it("CreateObject_CreateNewFile_Success", function(done) {
+    assert.equal(true, true);
+    done();
   });
+  it("DownloadObject_NominalCase_Success", function(done) {
+    assert.equal(true, true);
+    done();
+  });
+  it("IsObjectExists_NominalCase_Success", function(done) {
+    assert.equal(true, true);
+    done();
+  });
+  it("IsObjectExists_ObjectNotExistBucket_Success", function(done) {
+    assert.equal(true, true);
+    done();
+  });
+  it("IsObjectExists_ObjectNotExistFile_Success", function(done) {
+    assert.equal(true, true);
+    done();
+  });
+  it("RemoveObject_NominalCase_Success", function(done) {
+    assert.equal(true, true);
+    done();
+  });
+  afterEach(function() {});
 });
